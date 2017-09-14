b4274cda3d29b87f98bc742b82b61c055eb723e2
richardd3603 <31962277+richardd3603@users.noreply.github.com>
2017-09-14 16:29:13 -0400

    Add files via upload

---
+++ M2T1_SalesPrediction_RichardsDavid.py
@@ -0,0 +1,15 @@
+# CTI-110 
+# M2T1 - Sales Prediction 
+# David Richards
+# September 7, 2017
+# Calculate Sales Prediction
+
+# Get the projected total sales from user.
+total_sales = float (input ('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of the total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print ('The profit is $', format(profit, ',.2f'))
+
